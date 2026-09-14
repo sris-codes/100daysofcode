7d72c703eaff8fd4580d638e230f77df2b62874e
sris-codes <SRISTI.36481@stu.upes.ac.in>
2026-09-15 00:14:56 +0530

    Add files via upload

---
+++ DAY 2/Q-3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    float length, breadth, area, perimeter;
+
+    printf("Enter length: ");
+    scanf("%f", &length);
+
+    printf("Enter breadth: ");
+    scanf("%f", &breadth);
+
+    area = length * breadth;
+    perimeter = 2 * (length + breadth);
+
+    printf("Area of rectangle = %.2f\n", area);
+    printf("Perimeter of rectangle = %.2f\n", perimeter);
+
+    return 0;
+}
